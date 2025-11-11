531779e580cde9186744e19c3fcd0625cf921cb4
Olivier4508 <olivierdubfer@gmail.com>
2025-11-11 08:58:44 +0100

    11.11.2025 (Cours)

---
+++ 2_web/exemple.js
@@ -0,0 +1,29 @@
+console.log('bonjour');
+
+// var message = "bienvenue";
+
+// console.log(message);
+
+/// function ma_fonction() {
+//     console.log('ma_fonction');
+// }
+
+// ma_fonction(); 
+
+// for (var i = 0; i < 10; i++) {
+//    console.log(i);
+// }
+
+// console.log(document.querySelector('a'))
+
+// console.log(document.querySelector('.important').textContent)
+
+// console.log(document.getElementById('.important'))
+
+function ajouter_perso() {
+    var nom_perso = 'Jean';
+    var nouveau_li = document.createElement('li');
+    // var nouveau_text = document.createTextNode(text_personnage);
+
+    console.log(nom_perso);
+}
