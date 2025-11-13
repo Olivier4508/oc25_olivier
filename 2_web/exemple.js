d08df55dbad1b0a27c9a79a257048bc6371a7e31
Olivier4508 <olivierdubfer@gmail.com>
2025-11-13 15:20:56 +0100

    13.11.2025 (cours)

--- 2_web/exemple.js
+++ 2_web/exemple.js
@@ -1,4 +1,4 @@
-console.log('bonjour');
+// console.log('bonjour');
 
 // var message = "bienvenue";
 
@@ -20,10 +20,66 @@ console.log('bonjour');
 
 // console.log(document.getElementById('.important'))
 
-function ajouter_perso() {
-    var nom_perso = 'Jean';
-    var nouveau_li = document.createElement('li');
-    // var nouveau_text = document.createTextNode(text_personnage);
+// function ajouter_perso() {
+//    var nom_perso = 'Jean';
+//    var nouveau_li = document.createElement('li');
+//    var nouveau_text = document.createTextNode(text_personnage);
 
-    console.log(nom_perso);
-}
+//    console.log(nom_perso);
+// }
+
+// var jean = {
+//     nom: 'jean',
+//     classe: 'guerrier',
+//     niveau: 2,
+//     passer_niveau: function () {
+//         this.niveau++;
+//     },
+    
+// };
+
+// console.log(jean.niveau)
+// jean.passer_niveau();
+// console.log(jean.niveau)
+
+// for (var i = 0; i < troupe.length; i++) {
+//     var li_personnage = document.createElement('li');
+//     var texte_personnage = document.createTextNode(troupe[i].nom);
+//     li_personnage.appendChild(texte_personnage);
+//     var liste_perso = document.getElementById('liste_perso');
+//     liste_perso.appendChild(li_personnage);
+// }
+
+function creer_personnage(nom, classe, niveau) {
+    var nouveau_personnage = {
+        nom: nom,
+        classe: classe,
+        niveau: niveau,
+        passer_niveau: function () {
+            this.niveau++;
+        },
+        creer_li: function() {
+        var li_personnage = document.createElement('li');
+        var texte_personnage = document.createTextNode(
+            this.nom + ' (' + this.classe + ', niveau ' + this.niveau + ')'
+        );
+        li_personnage.appendChild(texte_personnage);
+        li_personnage.setAttribute('class', this.classe);
+        return li_personnage;
+    }
+    };
+    return nouveau_personnage
+};
+
+var troupe = [
+    creer_personnage('jean', 'guerrier', 2),
+    creer_personnage('nicole', 'voleur', 1),
+    creer_personnage('matteo', 'magicien', 4)
+];
+
+for (var i = 0; i < troupe.length; i++) {
+    var perso = troupe[i];
+    li_personnage = perso.creer_li();
+    var liste_perso = document.getElementById('liste_perso');
+    liste_perso.appendChild(li_personnage);
+};
