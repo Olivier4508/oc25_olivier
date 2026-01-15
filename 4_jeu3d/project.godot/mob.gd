a53c10749ca44605614ef9a752b37d82e04111eb
Olivier DF <olivierdubfer@gmail.com>
2026-01-15 15:21:02 +0100

    15/1/26 cours tutoriel 2d

---
+++ 4_jeu3d/project.godot/mob.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+@export var min_speed = 150 # Minimum speed range.
+@export var max_speed = 250 # Maximum speed range.
+
+var mob_types = ["walk", "swim", "fly"]
+
+func _ready():
+		$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	
+	
+
+func _on_visibility_screen_exited() -> void:
+	pass # Replace with function body.
+
+func _on_Visibility_screen_exited():
+	queue_free()
