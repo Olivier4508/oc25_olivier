e499182f798204f820301630107128e700265b69
Olivier DF <olivierdubfer@gmail.com>
2026-01-24 17:48:31 +0100

    finished 3d game at home 24/1/26

--- 4_jeu3d/tutoriel3d/mob.gd
+++ 4_jeu3d/tutoriel3d/mob.gd
@@ -1,10 +1,12 @@
 extends CharacterBody3D
 
-signal squashed
 # Minimum speed of the mob in meters per second.
 @export var min_speed = 10
 # Maximum speed of the mob in meters per second.
 @export var max_speed = 18
+
+# Emitted when the player jumped on the mob
+signal squashed
 
 func _physics_process(_delta):
 	move_and_slide()
@@ -17,6 +19,7 @@ func initialize(start_position, player_position):
 	# Rotate this mob randomly within range of -45 and +45 degrees,
 	# so that it doesn't move directly towards the player.
 	rotate_y(randf_range(-PI / 4, PI / 4))
+
 	# We calculate a random speed (integer)
 	var random_speed = randi_range(min_speed, max_speed)
 	# We calculate a forward velocity that represents the speed.
@@ -25,10 +28,11 @@ func initialize(start_position, player_position):
 	# in order to move in the direction the mob is looking.
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
+	$AnimationPlayer.speed_scale = random_speed / min_speed
 
-func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+func _on_visible_on_screen_notifier_3d_screen_exited():
 	queue_free()
-	
+
 func squash():
 	squashed.emit()
-	queue_free()
+	queue_free() # Destroy this node
--- 4_jeu3d/tutoriel3d/player.gd
+++ 4_jeu3d/tutoriel3d/player.gd
@@ -1,63 +1,64 @@
 extends CharacterBody3D
 
 signal hit
-# How fast the player moves in mters per second.
+
+# How fast the player moves in meters per second
 @export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
-@export var fall_accleration = 75
+# The downward acceleration while in the air, in meters per second squared.
+@export var fall_acceleration = 75
 # Vertical impulse applied to the character upon jumping in meters per second.
 @export var jump_impulse = 20
-# Vertical impulse applied to the character upon bouncing over a mob in meters per second.
+# Vertical impulse applied to the character upon bouncing over a mob
+# in meters per second.
 @export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
+
 func _physics_process(delta):
-	# We create a local variable to store the input direction.
+	# We create a local variable to store the input direction
 	var direction = Vector3.ZERO
-	
-	# We check for each move input and update the direction accordingly.
+
+	# We check for each move input and update the direction accordingly
 	if Input.is_action_pressed("move_right"):
-		direction.x += 1
+		direction.x = direction.x + 1
 	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
+		direction.x = direction.x - 1
 	if Input.is_action_pressed("move_back"):
 		# Notice how we are working with the vector's x and z axes.
 		# In 3D, the XZ plane is the ground plane.
-		direction.z += 1
+		direction.z = direction.z + 1
 	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-	
+		direction.z = direction.z - 1
+
+	# Prevent diagonal moving fast af
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
 		$Pivot.basis = Basis.looking_at(direction)
-	
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
+
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-	
+
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_accleration * delta)
-	
-	# Moving the Character
-	velocity = target_velocity
-	move_and_slide()
-	
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Jumping.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		target_velocity.y = jump_impulse
-		
-# Iterate through all collisions that occurred this frame
+
+	# Iterate through all collisions that occurred this frame
+	# in C this would be for(int i = 0; i < collisions.Count; i++)
 	for index in range(get_slide_collision_count()):
 		# We get one of the collisions with the player
 		var collision = get_slide_collision(index)
 
-		# If there are duplicate collisions with a mob in a single frame
-		# the mob will be deleted after the first collision, and a second call to
-		# get_collider will return null, leading to a null pointer when calling
-		# collision.get_collider().is_in_group("mob").
-		# This block of code prevents processing duplicate collisions.
+		# If the collision is with ground
 		if collision.get_collider() == null:
 			continue
 
@@ -72,9 +73,16 @@ func _physics_process(delta):
 				# Prevent further duplicate calls.
 				break
 
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
+	
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
+
+# And this function at the bottom.
 func die():
 	hit.emit()
 	queue_free()
 
-func _on_mob_detector_body_entered(body: Node3D) -> void:
+func _on_mob_detector_body_entered(body):
 	die()
